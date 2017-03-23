7c15ee1fc18ae6f73ff8321c0f4f4fafbe967a36
Grzegorz Mandziak <alimek@alimek.pl>
2017-03-23 23:45:26 +0100

    update example with counter

--- Example/index.ios.js
+++ Example/index.ios.js
@@ -13,35 +13,44 @@ import {
 
 import ReactNativeTooltipMenu from 'react-native-tooltip-menu';
 
-const Example = () => (
-  <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'center', padding: 25 }}>
-    <View>
-      <Text style={{ textAlign: 'center' }}>This is example of react-native-tooltip-menu</Text>
-    </View>
-    <ReactNativeTooltipMenu
-      buttonComponent={
-        <View
-          style={{
-            backgroundColor: 'purple',
-            padding: 10,
-            borderRadius: 25
-          }}
-        >
-          <Text style={{ color: 'white', flex: 1 }}>Click me to show tooltip!</Text>
+class Example extends Component {
+  state = {
+    counter: 0,
+  };
+
+  render() {
+    return (
+      <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'center', padding: 25 }}>
+        <View>
+          <Text style={{ textAlign: 'center' }}>This is example of react-native-tooltip-menu</Text>
+          <Text style={{ textAlign: 'center' }}>Clicked: {this.state.counter}</Text>
         </View>
-      }
-      items={[
-        {
-          label: 'Label #1',
-          onPress: () => {},
-        },
-        {
-          label: 'Label #2',
-          onPress: () => {},
-        },
-      ]}
-    />
-  </View>
-);
+        <ReactNativeTooltipMenu
+          buttonComponent={
+            <View
+              style={{
+                backgroundColor: 'purple',
+                padding: 10,
+                borderRadius: 25
+              }}
+            >
+              <Text style={{ color: 'white', flex: 1 }}>Click me to show tooltip!</Text>
+            </View>
+          }
+          items={[
+            {
+              label: 'Label #1',
+              onPress: () => this.setState({ counter: (this.state.counter + 1) })
+            },
+            {
+              label: 'Label #2',
+              onPress: () => this.setState({ counter: (this.state.counter + 1) }),
+            },
+          ]}
+        />
+      </View>
+    )
+  }
+}
 
 AppRegistry.registerComponent('Example', () => Example);
