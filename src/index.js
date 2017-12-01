d182da2a6b79c1e0bc485009ed59aae3a8f46971
Jeffrey Mendez <jeffreymendez@jeffreys-mbp.jerue.com>
2017-12-01 11:30:23 -0500

    updating android coordS

--- src/index.js
+++ src/index.js
@@ -108,6 +108,7 @@ class PopoverTooltip extends React.PureComponent<Props, State> {
     super(props);
     this.state = {
       isModalOpen: false,
+      androidY: 0,
       x: 0,
       y: 0,
       width: 0,
@@ -124,6 +125,13 @@ class PopoverTooltip extends React.PureComponent<Props, State> {
       tooltipContainerY: undefined,
       buttonComponentOpacity: 0,
     };
+  }
+
+  componentWillUpdate(nextProps, nextState){
+    if (Platform.OS === 'anroid' && !this.gotAndroidY) {
+        this.setState({androidY: nextState.y});
+        this.gotAndroidY = true;
+    }
   }
 
   componentWillMount() {
@@ -234,7 +242,7 @@ class PopoverTooltip extends React.PureComponent<Props, State> {
       ],
     };
 
-    const posYDistance = Platform.OS === 'ios' ? this.state.y : this.state.y - 20;
+    const posYDistance = this.state.androidY ? this.state.androidY : this.state.y;
 
     const items = this.props.items.map((item, index) => {
       const classes = [ this.props.labelContainerStyle ];
